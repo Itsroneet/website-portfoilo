d977facc6952d3b095e059d475ac751358aa3edd
Itsroneet <151948280+Itsroneet@users.noreply.github.com>
2024-03-29 15:08:56 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Wait for the page to load completely
+window.addEventListener('load', function() {
+    // Hide the preloader when the page is fully loaded
+    document.querySelector('.loading-screen').style.display = 'none';
+  });
+  
